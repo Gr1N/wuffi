837ea6a7254a70274b591ff66410dad750198786
Nikita Grishko <grin.minsk@gmail.com>
2016-04-03 12:00:16 +0300

    Initialized `wuffi` package

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: clean
+.PHONY: clean develop
+
+PACKAGE=wuffi
 
 help:
 	@echo "Please use 'make <target>' where <target> is one of"
 	@echo "  clean       => to clean clean all automatically generated files"
+	@echo "  develop     => to install $(PACKAGE) in 'development mode'"
 
 clean:
 	find src/ -name \*.pyc -delete
 	find src/ -name \*__pycache__ -delete
+
+develop:
+	python setup.py develop
---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from io import open
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wuffi',
+    version='0.0.0',
+    description='TBD',
+    long_description=open('README.md', encoding='utf-8').read(),
+    author='Nikita Grishko',
+    author_email='grin.minsk+github@gmail.com',
+    url='https://github.com/Gr1N/wuffi',
+    download_url='TBD',
+    license='MIT',
+    packages=find_packages(exclude=(
+        'tests.*',
+        'tests',
+        'example',
+    )),
+    install_requires=(
+    ),
+    extras_require={
+    },
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    )
+)
